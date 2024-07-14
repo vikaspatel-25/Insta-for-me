a07cafa92ecd6fa0d46daed8732bfa8429cafefa
Vikas Patel <vikaspatel.info.18@gmail.com>
2024-07-14 11:10:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const loading = document.querySelector("#loading");
 // API URLs for profiledata
 const urls = [
     'https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=pathsofstoicism&url_embed_safe=true',
-    'https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=bhuvan.bam22&url_embed_safe=true',
+    'https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=anushkasharma&url_embed_safe=true',
     'https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=virat.kohli&url_embed_safe=true',
     'https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=viratkohlistuffs&url_embed_safe=true'
 ];
